4382f3396467af5cf43e6cfcb7a1a7cf718b3f54
Nikola Trkulja <niktrk@gmail.com>
2021-03-15 22:13:22 +0100

    Vežbe 2

--- src/topic2_image_processing/filters/displacement/FlipVertical.java
+++ src/topic2_image_processing/filters/displacement/FlipVertical.java
@@ -11,7 +11,7 @@ public class FlipVertical extends DisplacementFilter {
 	@Override
 	public Vector source(Vector dst, Vector dim) {
 		// Vracamo poziciju koja je osno simetricna u odosu na srednju horizontalu.
-		return null;
+		return new Vector(dst.x, dim.y - 1 - dst.y);
 	}
 	
 }
